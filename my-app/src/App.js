803df81d1958087a422114a30e5ba56d3ff48077
Mario Oliveira <mario.oliveirag12@gmail.com>
2022-01-14 11:58:35 -0300

    adicionar conteudo

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadatro";
+import '@fontsource/roboto/300.css'
+import {Container, Typography } from "@mui/material"
+
+
+class App extends Component {
+  render() {
+    return ( 
+      <Container component="article" maxWidth="sm">
+         <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography> 
+         <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF}  />
+      </Container>
+      
+    );
+
+  }
+}
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
+}
+
+
+
+export default App;
